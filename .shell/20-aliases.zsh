40e4020de87ee3ccde91e21b03435864d890043a
Edvinas Aleksejonokas <Edvinas.Aleksejonokas@oxid-esales.com>
2016-06-25 00:10:13 +0300

    Add more aliases for lxc

--- .shell/20-aliases.zsh
+++ .shell/20-aliases.zsh
@@ -22,9 +22,15 @@ alias dockerenv='eval "$(docker-machine env default)"'
 alias vihosts='sudo vim /etc/hosts'
 
 alias cdd='cd /Volumes/Data/oxid/vms/lxc'
+alias cdbox='cd /Volumes/Data/oxid/vms/box'
 
-alias lxc='ssh -o LogLevel=quiet -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i /Volumes/Data/oxid/vms/lxc_box/.vagrant/machines/default/parallels/private_key vagrant@lxc'
+alias lxc='ssh -o LogLevel=quiet -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i /Volumes/Data/oxid/vms/box/.vagrant/machines/default/parallels/private_key vagrant@lxc'
 alias rdns='sudo killall -HUP mDNSResponder'
 
 alias dls='docker inspect -f "{{.Name}} - {{.NetworkSettings.IPAddress }}" $(docker ps -q)'
 alias dlsa='docker inspect -f "{{.Name}} - {{.NetworkSettings.IPAddress }}" $(docker ps -aq)'
+
+alias lxcroute='netstat -nr | grep 10.0.4 || sudo route -n add 10.0.3.0/24 192.168.33.10'
+alias mirror_lxc_hosts='(cat /etc/hosts_static ; lxc cat /etc/hosts | tail -n +8) | sudo bash -c "cat > /etc/hosts"'
+alias unmirror_lxc_hosts='sudo bash -c "cat /etc/hosts_static > /etc/hosts"'
+alias lxc-ls='lxc sudo lxc-ls -f'
