886e102e4eed3a4d243f84af1ff02dd724fac75e
Edvinas Aleksejonokas <Edvinas.Aleksejonokas@oxid-esales.com>
2016-10-03 15:33:54 +0300

    Include any version of fzf

--- .shell/10-fzf.zsh
+++ .shell/10-fzf.zsh
@@ -1,7 +1,7 @@
 export FZF_ROOT="$HOME/bin/fzf"
 
-. ~/homebrew/Cellar/fzf/0.12.0/shell/completion.zsh
-. ~/homebrew/Cellar/fzf/0.12.0/shell/key-bindings.zsh
+. ~/homebrew/Cellar/fzf/*/shell/completion.zsh
+. ~/homebrew/Cellar/fzf/*/shell/key-bindings.zsh
 
 alias gco='$FZF_ROOT/pass_thru.sh mirror_repo'
 alias gcoo='$FZF_ROOT/pass_thru.sh mirror_repo_oxid'
