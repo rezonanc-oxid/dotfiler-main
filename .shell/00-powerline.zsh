ffdf6dff692efa1ba49efabf5a8ed72b386916cf
Edvinas Aleksejonokas <Edvinas.Aleksejonokas@oxid-esales.com>
2016-10-23 19:15:49 +0300

    Adjust to new path of brew

--- .shell/00-powerline.zsh
+++ .shell/00-powerline.zsh
@@ -1,4 +1,4 @@
-P="$HOME/homebrew/lib/python2.7/site-packages/powerline/bindings/zsh/powerline.zsh"
+P="/usr/local/lib/python2.7/site-packages/powerline/bindings/zsh/powerline.zsh"
 
 if [[ -e $P ]]; then
   . $P
--- .shell/10-fzf.zsh
+++ .shell/10-fzf.zsh
@@ -1,7 +1,7 @@
 export FZF_ROOT="$HOME/bin/fzf"
 
-. ~/homebrew/Cellar/fzf/*/shell/completion.zsh
-. ~/homebrew/Cellar/fzf/*/shell/key-bindings.zsh
+. /usr/local/Cellar/fzf/*/shell/completion.zsh
+. /usr/local/Cellar/fzf/*/shell/key-bindings.zsh
 
 alias gco='$FZF_ROOT/pass_thru.sh mirror_repo'
 alias gcoo='$FZF_ROOT/pass_thru.sh mirror_repo_oxid'
